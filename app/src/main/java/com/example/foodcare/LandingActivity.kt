0139c76ae4313bb0002fc2ad752f62a69f702642
Jay <jy665593@dal.ca>
2020-03-13 00:16:34 -0300

    Created splash screen

---
+++ app/src/main/java/com/example/foodcare/LandingActivity.kt
@@ -0,0 +1,12 @@
+package com.example.foodcare
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LandingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_landing)
+    }
+}
