8b642c18c3f4eacd26a29181dcc20314f9742505
mjani <janimalav97@gmail.com>
2020-03-17 16:06:36 -0300

    Map integrated with marker

--- app/src/main/java/com/example/foodcare/MapActivity.kt
+++ app/src/main/java/com/example/foodcare/MapActivity.kt
@@ -3,7 +3,7 @@ package com.example.foodcare
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MapActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
