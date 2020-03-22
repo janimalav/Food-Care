9059ea1638f4145f736ad103517817614474463a
Asmita <as320435@dal.ca>
2020-03-21 21:04:53 -0300

    New Activity for profile edit page

--- app/src/main/java/com/example/foodcare/MainActivity.kt
+++ app/src/main/java/com/example/foodcare/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.foodcare
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        iv_edit_profile_back.setOnClickListener{
+            var intent = Intent(this, profile_page::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
---
+++ app/src/main/java/com/example/foodcare/profile_page.kt
@@ -0,0 +1,12 @@
+package com.example.foodcare
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class profile_page : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_page)
+    }
+}
