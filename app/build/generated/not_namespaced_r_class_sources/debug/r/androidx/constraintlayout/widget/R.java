2b23ff17654064cfbdf56fb0644d780abf69cfc8
Arjun K H <Arjun K H>
2020-04-01 19:44:29 -0300

    main screen changes

--- app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/constraintlayout/widget/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/constraintlayout/widget/R.java
@@ -74,19 +74,19 @@ public final class R {
         private id() {}
 
         public static final int bottom = 0x7f08004e;
-        public static final int end = 0x7f08007c;
-        public static final int gone = 0x7f080092;
-        public static final int invisible = 0x7f0800a0;
-        public static final int left = 0x7f0800a9;
-        public static final int packed = 0x7f0800df;
-        public static final int parent = 0x7f0800e1;
-        public static final int percent = 0x7f0800e6;
-        public static final int right = 0x7f0800f4;
-        public static final int spread = 0x7f08011e;
-        public static final int spread_inside = 0x7f08011f;
-        public static final int start = 0x7f080124;
-        public static final int top = 0x7f080146;
-        public static final int wrap = 0x7f080158;
+        public static final int end = 0x7f08007b;
+        public static final int gone = 0x7f080091;
+        public static final int invisible = 0x7f08009f;
+        public static final int left = 0x7f0800a8;
+        public static final int packed = 0x7f0800de;
+        public static final int parent = 0x7f0800e0;
+        public static final int percent = 0x7f0800e5;
+        public static final int right = 0x7f0800f3;
+        public static final int spread = 0x7f08011d;
+        public static final int spread_inside = 0x7f08011e;
+        public static final int start = 0x7f080123;
+        public static final int top = 0x7f080145;
+        public static final int wrap = 0x7f080157;
     }
     public static final class styleable {
         private styleable() {}
--- app/src/main/java/com/example/phase1proj/views/CategoryListActivity.kt
+++ app/src/main/java/com/example/phase1proj/views/CategoryListActivity.kt
@@ -3,6 +3,7 @@ package com.example.phase1proj.views
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageButton
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -14,12 +15,13 @@ import kotlinx.android.synthetic.main.categorylist.*
 class CategoryListActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var backButton: ImageButton
+    private lateinit var headTitile: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.categorylist)
         backButton = findViewById(R.id.backButton)
-
+        headTitile = findViewById(R.id.categoryListView)
         backButton.setOnClickListener(object : View.OnClickListener {
             override fun onClick(v: View?) {
                 this@CategoryListActivity.finish()
@@ -27,18 +29,19 @@ class CategoryListActivity : AppCompatActivity() {
 
         })
         val categoryName = intent.getStringExtra("categoryName")
+        headTitile.text = categoryName
         recyclerView = categoryListRecycler
         recyclerView.apply {
             layoutManager = GridLayoutManager(
-                this@CategoryListActivity,
-                2,
-                GridLayoutManager.VERTICAL,
-                false
+                    this@CategoryListActivity,
+                    2,
+                    GridLayoutManager.VERTICAL,
+                    false
             )
             adapter =
-                SpecificCategoryListAdapter(
-                    getCategoryList(categoryName, 40)
-                )
+                    SpecificCategoryListAdapter(
+                            getCategoryList(categoryName, 40)
+                    )
         }
     }
 
@@ -47,28 +50,28 @@ class CategoryListActivity : AppCompatActivity() {
         val children = mutableListOf<Vegetable>()
         repeat(count / 2) {
             children.add(
-                Vegetable(
-                    "Lobster Cooked in store",
-                    "Raw",
-                    R.drawable.veggie1,
-                    "New Veggie",
-                    20,
-                    45.20,
-                    "300g"
-                )
+                    Vegetable(
+                            "Lobster Cooked in store",
+                            "Raw",
+                            R.drawable.veggie1,
+                            "New Veggie",
+                            20,
+                            45.20,
+                            "300g"
+                    )
             )
         }
         repeat(count / 2) {
             children.add(
-                Vegetable(
-                    "Seasoned pork rack ribs end",
-                    "Raw",
-                    R.drawable.veggie2,
-                    "New Veggie",
-                    10,
-                    21.20,
-                    "400g"
-                )
+                    Vegetable(
+                            "Seasoned pork rack ribs end",
+                            "Raw",
+                            R.drawable.veggie2,
+                            "New Veggie",
+                            10,
+                            21.20,
+                            "400g"
+                    )
             )
         }
         return children
